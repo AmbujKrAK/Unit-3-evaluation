5aa707f187019c9f9049f4f5b09eb6aa3b0bd345
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-02-21 17:19:46 +0530

    Inital commit

---
+++ components/sidebar.js
@@ -0,0 +1,15 @@
+function sidebar() {
+
+
+    // return your html component here
+    //Make sure to give input search box id as ""
+    return `<a href="./index.html"><h2>Masai NEWS</h2></a>
+    <div>Login</div>
+    <input type="text" id="searchbar" placeholder="search news">
+    <h4>Startup</h4>
+    <div>Newsletters</div>
+    <h4>Audio</h4>
+    <h4>Video</h4>
+    `
+}
+export default sidebar
